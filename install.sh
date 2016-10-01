bda4ac2ecd0a6e655ce782f33f3046574d8b195e
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2016-09-30 22:06:34 -0400

    installs vim-plug as part of the install script

--- install.sh
+++ install.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-ln -sfhv $(pwd) ~/.vim > /dev/null 2>&1
-ln -sfhv ~/.vim/vimrc ~/.vimrc > /dev/null 2>&1
-vim -c "PlugInstall | qa"
+GREEN='\033[0;32m'
+NC='\033[0m'
 
-echo "vim-files successfuly installed!"
+echo "> Installing vim-files..."
+
+ln -sfhv $(pwd) ~/.vim >/dev/null 2>&1
+ln -sfhv ~/.vim/vimrc ~/.vimrc >/dev/null 2>&1
+echo -e "${GREEN}✓${NC} Symlinks created."
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim \
+  >/dev/null 2>&1
+echo -e "${GREEN}✓${NC} Vim-plug installed."
+
+vim -c "PlugInstall | qa"
+echo -e "${GREEN}✓${NC} Plugins installed."
+
+echo -e "${GREEN}✓${NC} Done."
